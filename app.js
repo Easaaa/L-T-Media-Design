611a05ac8d9e8b47e8f9a7dc30c98541d6b1fcb5
Leonardo Tononi <easaaa@protonmail.com>
2019-02-06 22:26:56 +0100

    Add JS scroll effect

---
+++ app.js
@@ -0,0 +1,54 @@
+// EFFECT PAGE
+
+window.sr = ScrollReveal();
+sr.reveal('.nav-bar', {
+duration: 3000,
+origin: 'top'
+});
+sr.reveal('.title-effect-left', {
+duration: 1500,
+origin: 'left',
+distance: '150px',
+viewFactor: 0.5
+});
+sr.reveal('.title-effect-right', {
+duration: 1500,
+origin: 'top',
+distance: '300px',
+viewFactor: 0.5
+});
+
+window.sr = ScrollReveal();
+sr.reveal('.scrollH', {
+duration: 2000,
+origin: 'top',
+distance: '300px'
+});
+
+sr.reveal('#services', {
+duration: 1000,
+origin: 'bottom',
+distance: '300px',
+viewFactor: 0.2
+});
+
+sr.reveal('#portfolio', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '150px',
+  viewFactor: 0.2
+  });
+
+sr.reveal('.infoL', {
+duration: 1300,
+origin: 'left',
+distance: '150px',
+viewFactor: 0.2
+});
+
+sr.reveal('.infoR', {
+duration: 1300,
+origin: 'right',
+distance: '150px',
+viewFactor: 0.2
+});
